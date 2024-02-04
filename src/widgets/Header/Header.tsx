27c14a550950e84381985c4d84ce4caaccf631e2
Нахатакян Артур Романович <artoim333@gmail.com>
2024-02-05 00:19:06 +0300

    styles: added Header

---
+++ src/widgets/Header/Header.tsx
@@ -0,0 +1,19 @@
+import Link from '@/shared/components/Typography/Links/Link/Link';
+
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <nav>
+                <Link href="/">home</Link>
+                <Link href="/blog">blog</Link>
+                <Link href="/projects">projects</Link>
+                <Link href="/cv">cv</Link>
+                <Link href="/contacts">contacts</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
